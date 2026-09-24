97ca3956fa93be545baa183c47361a44ff447b74
Bineth <binethellepola@gmail.com>
2026-09-24 08:34:01 +0530

    feat: add file upload middleware

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -2,14 +2,19 @@ const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "descriptions",
-    allowed_formats: ["jpg", "png", "jpeg"],
-  },
-});
+const createUpload = (folder) =>
+  multer({
+    storage: new CloudinaryStorage({
+      cloudinary: cloudinary,
+      params: {
+        folder: `ambalangoda_travels/${folder}`,
+        allowed_formats: ["jpg", "png", "jpeg", "webp"],
+      },
+    }),
+    limits: { fileSize: 8 * 1024 * 1024 },
+  });
 
-const upload = multer({ storage });
+const upload = createUpload("descriptions");
 
 module.exports = upload;
+module.exports.createUpload = createUpload;
